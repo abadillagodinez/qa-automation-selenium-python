fcb6744117718f4be56fce94bd152016056c24e8
Adrian Badilla <abadillagodinez@gmail.com>
2025-05-26 09:50:23 -0600

    Added script to test the interactions with the bootstrap carousel on Firefox

--- aux_files/manage_files.py
+++ aux_files/manage_files.py
@@ -4,7 +4,7 @@ import os
 # method to open the file with the test cases
 def get_test_cases(fileName):
     # path for ../data/fileName.json
-    test_cases_path = os.path.join(os.path.dirname(__file__),'..','data', fileName)
+    test_cases_path = os.path.join(os.path.dirname(__file__),'..','data/test_cases/', fileName)
     # open the file
     with open(test_cases_path) as f:
         test_cases = json.load(f)
